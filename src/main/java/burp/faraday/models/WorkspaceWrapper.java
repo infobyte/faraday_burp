caa8460f6a7a07980df365c10d5777fb7b73f821
Diego Nadares <dnadares@faradaysec.com>
2025-02-18 12:19:39 -0300

    Add WorskapceWrapper

---
+++ src/main/java/burp/faraday/models/WorkspaceWrapper.java
@@ -0,0 +1,12 @@
+package burp.faraday.models;
+
+import java.util.List;
+
+public class WorkspaceWrapper {
+    private List<Workspace> rows;
+    private int total;
+
+    public List<Workspace> getRows() {
+        return rows;
+    }
+}
